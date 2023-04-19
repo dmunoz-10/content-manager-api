f09595ce287fcef5c0bc71036292fd108a509a11
Daniel Muñoz <djmv225@gmail.com>
2023-04-18 23:45:27 -0500

    Sets up cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express, { type Application } from 'express';
+import cors, { type CorsOptions } from 'cors';
 
 import dataJson from './data.json';
 
 const app: Application = express();
 const PORT = 3001;
+const corsOptions: CorsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
