f069c25199dbaa941a61573ad5a907b710817aa3
Daniel Muñoz <djmv225@gmail.com>
2023-04-18 23:30:55 -0500

    Adds the resources endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express, { type Application } from 'express';
 
-const app: Application = express();
+import dataJson from './data.json';
 
+const app: Application = express();
 const PORT = 3001;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/resources', (_req, res) => {
+  res.status(200).json(dataJson);
 });
 
 app.listen(PORT, () => {
