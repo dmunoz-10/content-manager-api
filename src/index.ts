73b6419ab27613691aa9288b5f743ab0e32aed99
Daniel Muñoz <djmv225@gmail.com>
2023-04-18 22:43:31 -0500

    Removes pretty-quick in favor of lint-staged

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express, { Application } from 'express';
+import express, { type Application } from 'express';
 
 const app: Application = express();
 
-app.set('PORT', 3001);
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+const PORT = 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(app.get('PORT'), () => {
-  console.log(`Server is listening on port ${app.get('PORT')}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
