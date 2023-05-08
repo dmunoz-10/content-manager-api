6588707939417037cbb58d2ec4a906e3703ac514
Daniel Muñoz <djmv225@gmail.com>
2023-05-07 20:26:09 -0500

    Finishes the update endpoint when activating a resource

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { format } from 'date-fns';
 
 import dataJson from './data.json';
 
-import type { ResourceData, ResourceDataCreate } from '@/types';
+import type { ResourceData, ResourceDataCreate, ResourceDataUpdate } from '@/types';
 
 const resources = dataJson;
 
@@ -46,7 +46,7 @@ app.get('/api/resources/:id', (req, res) => {
   const { id: resourceId } = req.params;
   const resource: ResourceData | undefined = resources.find(({ id }) => resourceId === id);
 
-  if (resource !== undefined) {
+  if (resource) {
     res.status(200).json({ resource });
   } else {
     res.status(404).json({ error: 'Resource not found' });
@@ -57,13 +57,33 @@ app.put('/api/resources/:id', (req, res) => {
   const { id: resourceId } = req.params;
   const resource: ResourceData | undefined = resources.find(({ id }) => resourceId === id);
 
-  if (resource !== undefined) {
+  if (resource) {
+    const activeResource = resources.find(({ status }) => status === 'active');
+    const activeResourceIndex = resources.findIndex(({ id }) => activeResource?.id === id);
     const resourceIndex = resources.findIndex(({ id }) => resourceId === id);
-    const { id: _id, ...restData } = req.body as ResourceData;
-    resources[resourceIndex] = {
-      id: resourceId,
-      ...restData,
-    };
+    const data = req.body as ResourceDataUpdate;
+
+    if (data.status === 'active' && activeResource) {
+      if (activeResource.id === resourceId) {
+        return res.status(422).json({ error: "It's already activated!" });
+      } else {
+        resources[resourceIndex] = {
+          id: resourceId,
+          ...data,
+        };
+
+        resources[activeResourceIndex] = {
+          ...activeResource,
+          status: 'inactive',
+        };
+      }
+    } else {
+      resources[resourceIndex] = {
+        id: resourceId,
+        ...data,
+      };
+    }
+
     res.status(200).json({ message: 'Resource updated!' });
   } else {
     res.status(404).json({ error: 'Resource not found' });
--- src/types/index.ts
+++ src/types/index.ts
@@ -9,6 +9,16 @@ export type ResourceData = {
   createdAt: string;
 };
 
+export type ResourceDataUpdate = {
+  title: string;
+  description: string;
+  link: string;
+  priority: number;
+  timeToFinish: number;
+  status: string;
+  createdAt: string;
+};
+
 export type ResourceDataCreate = {
   title: string;
   description: string;
